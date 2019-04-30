3777f1cdd8bcdff21e9bb896e3b28b3904e2eb80
xiashanhao <xiashanhao@meishubao.com>
2019-04-30 15:50:37 +0800

    project init

---
+++ src/main/java/com/summer/excelhelp/annotation/ImportField.java
@@ -0,0 +1,9 @@
+package com.summer.excelhelp.annotation;
+
+/**
+ * @author xiashanhao
+ * @date 2019-04-30 15:49
+ */
+public @interface ImportField {
+
+}
---
+++ src/test/java/com.summer.excelhelp/BaseTest.java
@@ -0,0 +1,8 @@
+package com.summer.excelhelp;
+
+/**
+ * @author xiashanhao
+ * @date 2019-04-30 15:48
+ */
+public class BaseTest {
+}
